b938ae81b6be87aa406e753b3a171ad3f3c93a8d
ThyaraV <thyara.vintimilla@udla.edu.ec>
2024-01-21 17:39:10 -0500

    Config data

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
+import grades from './data/grade.js';
 
 const port=process.env.PORT ||5000;
+
+connectDB();
+
 const app=express();
 
 app.get('/',(req,res)=>{
     res.send('API is running...')
+});
+
+app.get('/api/grades',(req,res)=>{
+    res.send(grades);
 });
 
 app.listen(port,()=>console.log(`Server running on port ${port}`));
