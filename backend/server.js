448e3bddb652d14188ad15cc97c435127f9ea373
ThyaraV <thyara.vintimilla@udla.edu.ec>
2024-01-31 00:02:43 -0500

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -20,17 +21,26 @@ const app=express();
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('API is running...')
-});
 
 app.use('/api/grades',gradeRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/progresos',progresoRoutes);
 
+const __dirname=path.resolve();
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+    app.get('*',(req,res)=>
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}else{
+    app.get('/',(req,res)=>
+    res.end('API is running....'));
+}
+
 app.use(notFound);
 app.use(errorHandler);
-
 
 
 app.listen(port,()=>console.log(`Server running on port ${port}`));
